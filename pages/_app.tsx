9a15aa262e273d19aa6a734f1478a424bf587011
Vainius <vmykolaitis1@gmail.com>
2023-02-20 20:54:44 +0200

    First commit xoxo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { MantineProvider } from '@mantine/core';
+import mantineCache from '@/mantineCache';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      withGlobalStyles
+      withNormalizeCSS
+      emotionCache={mantineCache}
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
